727d1ed8946e72d647fb5db67660b08ee3184350
madaghafuri <mada.ghafuri@gmail.com>
2024-06-15 23:20:59 +0700

    feat: add tailwind

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./db/schema/*",
+  schema: "./src/db/schema/*",
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        stroke: "#EDEFF2",
+        primary: "#2A4713",
+        secondary: "#93D076",
+      },
+    },
+  },
+  plugins: [],
+};
