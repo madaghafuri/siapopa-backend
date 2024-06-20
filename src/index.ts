b381db41e60505767a638d29f104a418fadc48b7
madaghafuri <mada.ghafuri@gmail.com>
2024-06-20 11:12:02 +0700

    fix: tsconfig

--- src/index.ts
+++ src/index.ts
@@ -2,35 +2,40 @@ import { serve } from "@hono/node-server";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Hono } from "hono";
 import pg from "pg";
-import { api } from "./api";
-import * as user from "./db/schema/user";
+// import { Client } from "pg";
+import api from "./api/route.js";
+import * as user from "./db/schema/user.js";
 import { logger } from "hono/logger";
-import * as desa from "./db/schema/desa";
-import * as detailRumpun from "./db/schema/detail-rumpun";
-import * as kabupatenKota from "./db/schema/kabupaten-kota";
-import * as kategoriLaporan from "./db/schema/kategori-laporan";
-import * as kecamatan from "./db/schema/kecamatan";
-import * as laporanBulanan from "./db/schema/laporan-bulanan";
-import * as laporanHarian from "./db/schema/laporan-harian";
-import * as laporanMusiman from "./db/schema/laporan-musiman";
-import * as laporanSb from "./db/schema/laporan-sb";
-import * as lokasi from "./db/schema/lokasi";
-import * as luasKerusakanSb from "./db/schema/luas-kerusakan-sb";
-import * as makhlukAsing from "./db/schema/makhluk-asing";
-import * as opt from "./db/schema/opt";
-import * as pengamatan from "./db/schema/pengamatan";
-import * as photoPengamatan from "./db/schema/photo-pengamatan";
-import * as provinsi from "./db/schema/provinsi";
-import * as rumpun from "./db/schema/rumpun";
-import * as tanaman from "./db/schema/tanaman";
-import * as userGroup from "./db/schema/user-group";
-import * as validasiLaporan from "./db/schema/validasi-laporan";
-import { web } from "./web";
+import * as desa from "./db/schema/desa.js";
+import * as detailRumpun from "./db/schema/detail-rumpun.js";
+import * as kabupatenKota from "./db/schema/kabupaten-kota.js";
+import * as kategoriLaporan from "./db/schema/kategori-laporan.js";
+import * as kecamatan from "./db/schema/kecamatan.js";
+import * as laporanBulanan from "./db/schema/laporan-bulanan.js";
+import * as laporanHarian from "./db/schema/laporan-harian.js";
+import * as laporanMusiman from "./db/schema/laporan-musiman.js";
+import * as laporanSb from "./db/schema/laporan-sb.js";
+import * as lokasi from "./db/schema/lokasi.js";
+import * as luasKerusakanSb from "./db/schema/luas-kerusakan-sb.js";
+import * as makhlukAsing from "./db/schema/makhluk-asing.js";
+import * as opt from "./db/schema/opt.js";
+import * as pengamatan from "./db/schema/pengamatan.js";
+import * as photoPengamatan from "./db/schema/photo-pengamatan.js";
+import * as provinsi from "./db/schema/provinsi.js";
+import * as rumpun from "./db/schema/rumpun.js";
+import * as tanaman from "./db/schema/tanaman.js";
+import * as userGroup from "./db/schema/user-group.js";
+import * as validasiLaporan from "./db/schema/validasi-laporan.js";
+import web from "./web/route.js";
 import { serveStatic } from "@hono/node-server/serve-static";
 const { Client } = pg;
 
 const client = new Client({
-  connectionString: "postgres://postgres:postgres@db:5432/siapopa-dev",
+  user: "postgres",
+  password: "postgres",
+  host: "db",
+  port: 5432,
+  database: "siapopa-dev",
   ssl: false,
 });
 await client.connect();
