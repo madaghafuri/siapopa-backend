5547dcf193ecde556dcf37ef0d5d3f76944b55ba
Dinda <dindamesti@gmail.com>
2024-07-02 15:57:54 +0700

    feat: add numbering

--- src/web/pages/master/opt.tsx
+++ src/web/pages/master/opt.tsx
@@ -14,16 +14,18 @@ const DataOPT = ({ listOpt }: { listOpt: OptWithTanaman[] }) => {
       <table id="optTable" class="row-border" style="width:100%">
         <thead>
           <tr>
-            <th class="py-2 px-4 border-b border-gray-200">Kode OPT</th>
+            <th class="py-2 px-4 border-b border-gray-200" style="width: 5%">No.</th>
+            <th class="py-2 px-4 border-b border-gray-200" style="width: 15%">Kode OPT</th>
             <th class="py-2 px-4 border-b border-gray-200">Nama OPT</th>
             <th class="py-2 px-4 border-b border-gray-200">Status</th>
             <th class="py-2 px-4 border-b border-gray-200">Tanaman</th>
           </tr>
         </thead>
         <tbody>
-          {listOpt.map((opt) => (
+          {listOpt.map((opt, index) => (
             <tr key={opt.kode_opt}>
-              <td class="py-2 px-4 border-b border-gray-200">{opt.kode_opt}</td>
+              <td class="py-2 px-4 border-b border-gray-200" style="width: 5%">{index + 1}</td>
+              <td class="py-2 px-4 border-b border-gray-200" style="width: 15%">{opt.kode_opt}</td>
               <td class="py-2 px-4 border-b border-gray-200">{opt.nama_opt}</td>
               <td class="py-2 px-4 border-b border-gray-200">{opt.status === 'mutlak' ? 'Mutlak' : 'Tidak Mutlak'}</td>
               <td class="py-2 px-4 border-b border-gray-200">{opt.nama_tanaman}</td>
