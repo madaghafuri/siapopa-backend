9d77ecac4d306349f8d3c058af99dc2ba0436b29
madaghafuri <mada.ghafuri@gmail.com>
2024-07-12 12:48:04 +0700

    feat: add new color for prettier config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
-        stroke: "#EDEFF2",
-        primary: "#2A4713",
-        secondary: "#93D076",
+        stroke: '#EDEFF2',
+        primary: '#2A4713',
+        secondary: '#93D076',
+        background: '#ecf0f5',
       },
     },
   },
